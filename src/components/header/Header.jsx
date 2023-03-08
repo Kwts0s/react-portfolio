4355ceb9c5c0a64cdf48ede8b0c6e3333a5265e3
Kwts0s <zeus.primers@gmail.com>
2023-03-08 18:15:59 +0200

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Kostas Galliakis</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA/>
+        <HeaderSocial/>
+
+        <div className="me">
+          <img src={ME} alt="person" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+      <a href="https://www.linkedin.com/in/konstantinos-galliakis-243061210/" target="_blank"><BsLinkedin/></a>
+      <a href="https://github.com/Kwts0s" target="_blank"><FaGithub/></a>
+      
+
+    </div>
+  )
+}
+
+export default HeaderSocials
