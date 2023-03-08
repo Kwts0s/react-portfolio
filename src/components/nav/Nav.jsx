a03e2a34ac2e15ae7fc1f87e4e57fe72ab25553f
Kwts0s <zeus.primers@gmail.com>
2023-03-08 19:23:22 +0200

    Navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './nav.css'
+import {FiHome} from 'react-icons/fi'
+import {BiUser} from 'react-icons/bi'
+import {BiBook} from 'react-icons/bi'
+import {MdOutlineDesignServices} from 'react-icons/md'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import { useState } from 'react'
+
 
 const Nav = () => {
+  const [activeNav,setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav=== '#' ? 'active' : ''}><FiHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav=== '#about' ? 'active' : ''}><BiUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav=== '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav=== '#services' ? 'active' : ''}><MdOutlineDesignServices/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav=== '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
